608c047a043f79984e8e46a6b0298827a35ae215
Rohanpatkal <rohanpatkal99@gmail.com>
2025-05-04 11:51:40 +0530

    ajkjd

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// File: /server/index.js
+/*// File: /server/index.js
 const express = require('express');
 const http = require('http');
 const socketIO = require('socket.io');
@@ -42,6 +42,65 @@ io.on('connection', socket => {
       players[socket.id].bullets.push(bullet);
     }
     io.emit('players-update', players);
+  });
+
+  socket.on('disconnect', () => {
+    delete players[socket.id];
+    io.emit('player-disconnect', socket.id);
+    console.log('Player disconnected:', socket.id);
+  });
+});
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+*/
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server, {
+  cors: {
+    origin: '*',
+  }
+});
+
+const PORT = 5000;
+let players = {};
+
+io.on('connection', socket => {
+  console.log('Player connected:', socket.id);
+
+  players[socket.id] = {
+    id: socket.id,
+    x: Math.random() * 500,
+    y: Math.random() * 500,
+    angle: 0,
+    bullets: []
+  };
+
+  socket.emit('init', players);
+  socket.broadcast.emit('new-player', players[socket.id]);
+
+  socket.on('move', data => {
+    if (players[socket.id]) {
+      players[socket.id].x = data.x;
+      players[socket.id].y = data.y;
+      players[socket.id].angle = data.angle;
+    }
+    io.emit('players-update', players);
+  });
+
+  socket.on('shoot', bullet => {
+    if (players[socket.id]) {
+      players[socket.id].bullets.push(bullet);
+    }
+    io.emit('players-update', players);
+  });
+
+  // ✅ Latency test handler
+  socket.on('ping-check', (clientTimestamp) => {
+    socket.emit('pong-check', clientTimestamp);
   });
 
   socket.on('disconnect', () => {
