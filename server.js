41fbfe11f9bd9e5566d832786d13558d513427d0
Rohanpatkal <rohanpatkal99@gmail.com>
2025-05-04 12:00:54 +0530

    adaf

--- server.js
+++ server.js
@@ -60,54 +60,23 @@ const socketIO = require('socket.io');
 const app = express();
 const server = http.createServer(app);
 const io = socketIO(server, {
-  cors: {
-    origin: '*',
-  }
+  cors: { origin: '*' }
 });
 
 const PORT = 5000;
-let players = {};
 
 io.on('connection', socket => {
-  console.log('Player connected:', socket.id);
+  console.log('Client connected:', socket.id);
 
-  players[socket.id] = {
-    id: socket.id,
-    x: Math.random() * 500,
-    y: Math.random() * 500,
-    angle: 0,
-    bullets: []
-  };
-
-  socket.emit('init', players);
-  socket.broadcast.emit('new-player', players[socket.id]);
-
-  socket.on('move', data => {
-    if (players[socket.id]) {
-      players[socket.id].x = data.x;
-      players[socket.id].y = data.y;
-      players[socket.id].angle = data.angle;
-    }
-    io.emit('players-update', players);
-  });
-
-  socket.on('shoot', bullet => {
-    if (players[socket.id]) {
-      players[socket.id].bullets.push(bullet);
-    }
-    io.emit('players-update', players);
-  });
-
-  // ✅ Latency test handler
-  socket.on('ping-check', (clientTimestamp) => {
-    socket.emit('pong-check', clientTimestamp);
+  socket.on('ping-check', (timestamp) => {
+    socket.emit('pong-check', timestamp);
   });
 
   socket.on('disconnect', () => {
-    delete players[socket.id];
-    io.emit('player-disconnect', socket.id);
-    console.log('Player disconnected:', socket.id);
+    console.log('Client disconnected:', socket.id);
   });
 });
 
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+server.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
